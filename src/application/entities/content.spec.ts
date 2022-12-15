ddc25a28adb1362d4b38947abad1156db8c25f11
Robert Andrade <robert_andrade@icloud.com>
2022-12-15 10:55:13 -0300

    teste unitario com as notificações

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,15 +1,18 @@
 import { Content } from './content';
 
-test('it should be able to create a notification content', () => {
-  const content = new Content('Você recebeu uma nova notificação de amizade');
+describe('Notification content', () => {
+    it('should be able to create a notification content', () => {
+      const content = new Content('Você recebeu uma nova notificação de amizade');    
+      expect(content).toBeTruthy();
+    });
+    
+    it('should not be able to create a notification content with less than 5 characteres', () => {
+      expect(() => new Content('aaa')).toThrow();
+    });
+    
+    it('should not be able to create a notification content with or more 240 characteres', () => {
+      expect(() => new Content('a'.repeat(241))).toThrow();
+    });
 
-  expect(content).toBeTruthy();
-});
+})
 
-test('it should not be able to create a notification content with less than 5 characteres', () => {
-  expect(() => new Content('aaa')).toThrow();
-});
-
-test('it should not be able to create a notification content with or more 240 characteres', () => {
-  expect(() => new Content('a'.repeat(241))).toThrow();
-});
